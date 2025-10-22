2c1c893f44d7bda502fb375f07dbc9b7ad5f03ed
METEOPuT <119122878+METEOPuT@users.noreply.github.com>
2025-10-23 00:57:52 +0300

    Add files

---
+++ Library.cs
@@ -0,0 +1,105 @@
+﻿
+namespace AnimalLibrary
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public class CommentAttribute : Attribute
+    {
+        public string Comment { get; }
+
+        public CommentAttribute(string comment)
+        {
+            Comment = comment;
+        }
+    }
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+    [Comment("Абстрактный базовый класс для всех животных")]
+    public abstract class Animal
+    {
+        public string Country { get; set; }
+        public bool HideFromOtherAnimals { get; set; }
+        public string Name { get; set; }
+        public string WhatAnimal { get; set; }
+        protected Animal(string country, bool hide, string name, string whatAnimal)
+        {
+            Country = country;
+            HideFromOtherAnimals = hide;
+            Name = name;
+            WhatAnimal = whatAnimal;
+        }
+        public void Deconstruct(out string country, out bool hide, out string name, out string whatAnimal)
+        {
+            country = Country;
+            hide = HideFromOtherAnimals;
+            name = Name;
+            whatAnimal = WhatAnimal;
+        }
+        public abstract eClassificationAnimal GetClassificationAnimal();
+        public abstract eFavouriteFood GetFavouriteFood();
+        public abstract void SayHello();
+    }
+    [Comment("Класс коровы - травоядное животное")]
+    public class Cow : Animal
+    {
+        public Cow(string country, bool hide, string name)
+            : base(country, hide, name, "Cow") { }
+        public override eClassificationAnimal GetClassificationAnimal()
+        {
+            return eClassificationAnimal.Herbivores;
+        }
+        public override eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Plants;
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine("Приветствие: Муууу");
+        }
+    }
+    [Comment("Класс льва - хищное животное")]
+    public class Lion : Animal
+    {
+        public Lion(string country, bool hide, string name)
+            : base(country, hide, name, "Lion") { }
+        public override eClassificationAnimal GetClassificationAnimal()
+        {
+            return eClassificationAnimal.Carnivores;
+        }
+        public override eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Meat;
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine("Приветствие: Рррррр");
+        }
+    }
+    [Comment("Класс свиньи - всеядное животное")]
+    public class Pig : Animal
+    {
+        public Pig(string country, bool hide, string name)
+            : base(country, hide, name, "Pig") { }
+        public override eClassificationAnimal GetClassificationAnimal()
+        {
+            return eClassificationAnimal.Omnivores;
+        }
+        public override eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Everything;
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine("Приветствие: Хрю-хрю-хрю");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using AnimalLibrary;
+using System;
+using System.Reflection;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
+        XDocument xmlDoc = new XDocument(
+            new XElement("Classes")
+        );
+
+        foreach (Type type in assembly.GetTypes())
+        {
+            if (type.IsClass && (type == typeof(Animal) || type.IsSubclassOf(typeof(Animal))))
+            {
+                XElement classElement = new XElement("Class",
+                    new XAttribute("Name", type.Name)
+                );
+                var commentAttr = type.GetCustomAttribute<CommentAttribute>();
+                if (commentAttr != null)
+                {
+                    classElement.Add(new XElement("Comment", commentAttr.Comment));
+                }
+
+                XElement propertiesElement = new XElement("Properties");
+                foreach (var prop in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    propertiesElement.Add(new XElement("Property",
+                        new XAttribute("Name", prop.Name),
+                        new XAttribute("Type", prop.PropertyType.Name)
+                    ));
+                }
+                classElement.Add(propertiesElement);
+
+                XElement methodsElement = new XElement("Methods");
+                foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+                {
+                    if (!method.IsSpecialName)
+                    {
+                        methodsElement.Add(new XElement("Method",
+                            new XAttribute("Name", method.Name),
+                            new XAttribute("ReturnType", method.ReturnType.Name)
+                        ));
+                    }
+                }
+                classElement.Add(methodsElement);
+
+                xmlDoc.Root.Add(classElement);
+            }
+        }
+
+        xmlDoc.Save("classes.xml");
+        Console.WriteLine("XML файл создан: classes.xml");
+    }
+}
